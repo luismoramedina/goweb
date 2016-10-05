bea3f30f946b2065478e2958118db30ff49e34f7
luismoramedina <luismoramedina@gmail.com>
2016-10-05 09:30:25 +0200

    get port from env

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
 func main() {
+    port := os.Getenv("PORT")
     http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":80", nil)
+    fmt.Println("listening... on " + port)
+    err := http.ListenAndServe(":" + port, nil)
     if err != nil {
       panic(err)
     }
